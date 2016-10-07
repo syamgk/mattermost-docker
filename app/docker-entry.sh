f85dd389e1a7f7dd17301058501af52b171b83ac
Syam.G.Krishnan <syamgk01@gmail.com>
2016-10-07 16:07:51 +0530

    remove if statement

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 config=/mattermost/config/config.json
 DB_HOST=${DB_HOST:-db}
@@ -5,18 +6,14 @@ DB_PORT_5432_TCP_PORT=${DB_PORT_5432_TCP_PORT:-5432}
 MM_USERNAME=${MM_USERNAME:-mmuser}
 MM_PASSWORD=${MM_PASSWORD:-mmuser_password}
 MM_DBNAME=${MM_DBNAME:-mattermost}
+
 echo -ne "Configure database connection..."
-if [ ! -f $config ]
-then
-    sed -Ei "s/DB_HOST/$DB_HOST/" $config
-    sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
-    sed -Ei "s/MM_USERNAME/$MM_USERNAME/" $config
-    sed -Ei "s/MM_PASSWORD/$MM_PASSWORD/" $config
-    sed -Ei "s/MM_DBNAME/$MM_DBNAME/" $config
-    echo OK
-else
-    echo SKIP
-fi
+sed -Ei "s/DB_HOST/$DB_HOST/" $config
+sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
+sed -Ei "s/MM_USERNAME/$MM_USERNAME/" $config
+sed -Ei "s/MM_PASSWORD/$MM_PASSWORD/" $config
+sed -Ei "s/MM_DBNAME/$MM_DBNAME/" $config
+echo OK
 
 echo "Wait until database $DB_HOST:$DB_PORT_5432_TCP_PORT is ready..."
 until nc $DB_HOST $DB_PORT_5432_TCP_PORT
