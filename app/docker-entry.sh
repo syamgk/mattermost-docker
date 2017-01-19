1c4d1dd03aab96e4dc696869006cbac9e535621d
Syam G Krishnan <sgk@riseup.net>
2017-01-19 17:57:18 +0530

    Add jq
    add jq and made the docker-entry script more verbose

--- app/docker-entry.sh
+++ app/docker-entry.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 config=/mattermost/config/config.json
-DB_HOST=${DB_HOST:-db}
-DB_PORT_5432_TCP_PORT=${DB_PORT_5432_TCP_PORT:-5432}
-MM_USERNAME=${MM_USERNAME:-mmuser}
-MM_PASSWORD=${MM_PASSWORD:-mmuser_password}
-MM_DBNAME=${MM_DBNAME:-mattermost}
-MM_MIGRATE=${MM_MIGRATE:-no}
 
-if [[ $MM_MIGRATE == "no" ]]; then 
-curl https://raw.githubusercontent.com/syamgk/mattermost-docker/master/app/config.template.json > $config;
+function updatejson() {
+  set -o nounset
+  key=$1
+  value=$2
+  file=$3
+  jq "$key = \"$value\"" $file | awk 'BEGIN{RS="";getline<"-";print>ARGV[1]}' $file
+  echo "Updated file $file"
+  set +o nounset
+}
+
+if [ -f "$config" ]
+then
+    echo "old $config found."
+else
+    echo "$config not found."
+    curl https://raw.githubusercontent.com/syamgk/mattermost-docker/master/app/config.template.json > $config;
+    echo -ne "Updating mattermost configuration..."
+    if [[ -z $DB_HOST ]]; then echo "DB_HOST not set."; exit 1; fi
+    if [[ -z $DB_PORT ]]; then echo "DB_PORT not set."; exit 1; fi
+    if [[ -z $MM_USERNAME ]]; then echo "MM_USERNAME not set."; exit 1; fi
+    if [[ -z $MM_PASSWORD ]]; then echo "MM_PASSWORD not set."; exit 1; fi
+    if [[ -z $MM_DBNAME ]]; then echo "MM_DBNAME not set."; exit 1; fi
+    updatejson ".SqlSettings.DriverName" "postgres" $config
+    updatejson ".SqlSettings.DataSource" "postgres://${MM_USERNAME}:${MM_PASSWORD}@${DB_HOST}:${DB_PORT}/${MM_DBNAME}?sslmode=disable&connect_timeout=10" $config
 fi
 
-echo -ne "Configure database connection..."
-sed -Ei "s/DB_HOST/$DB_HOST/" $config
-sed -Ei "s/DB_PORT/$DB_PORT_5432_TCP_PORT/" $config
-sed -Ei "s/MM_USERNAME/$MM_USERNAME/" $config
-sed -Ei "s/MM_PASSWORD/$MM_PASSWORD/" $config
-sed -Ei "s/MM_DBNAME/$MM_DBNAME/" $config
-echo OK
-
-echo "Wait until database $DB_HOST:$DB_PORT_5432_TCP_PORT is ready..."
-until nc $DB_HOST $DB_PORT_5432_TCP_PORT
-do
-    sleep 1
+timeout=30
+curtime=0
+while ! echo | nc -w1 $DB_HOST $DB_PORT > /dev/null 2>&1; do
+  echo "Waiting ${timeout}s for database to be available at ${DB_HOST}:${DB_PORT}... ${curtime}s"
+  sleep 1
+  curtime=$(($curtime+1))
+  if [[ $curtime -ge $timeout ]]; then
+    echo "Could not connect to the database. Aborting after $timeout seconds."
+    exit 1
+  fi  
 done
 # Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
 sleep 1
